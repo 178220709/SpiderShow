7d9cd92f2116f1df28d10a9811baba0639c2ce73
jsonsong <sj178220709@163.com>
2016-01-14 18:37:26 +0800

    cfg

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 var os = require('os');
 
-config.entry.unshift('webpack-dev-server/client?http://localhost:8090', "webpack/hot/dev-server");
+config.entry.unshift('webpack-dev-server/client?http://localhost:4072', "webpack/hot/dev-server");
+
+
 config.plugins.push(new webpack.HotModuleReplacementPlugin());
 
 
@@ -19,7 +21,7 @@ else if (os.hostname() == 'DESKTOP-92VGR1C') {
 }
 
 
-// 这里配置：请求http://localhost:9090/api，
+// 这里配置：请求 local /api，
 // 相当于通过本地node服务代理请求到了http://cnodejs.org/api
 var proxy = [{
     path: "/api/*",
